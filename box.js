c7980a75527af703d45b677b086109f4780d94c7
Mario Magdy <mariomagdyfa@gmail.com>
2023-03-12 20:47:27 +0200

    changed

--- box.js
+++ box.js
@@ -5,22 +5,47 @@ const content1=document.querySelector(".example1.content1");
 const title2=document.querySelector(".example2.title2");
 const content2=document.querySelector(".example2.content2");
 let image=document.getElementById("img1");
-let img1="Images/DSC06832_3.webp";
 let img0="Images/accordion__1_.jpg";
+let imgs="Images/DSC06832_3.webp";
+let imgs1="Images/Creative_Life_Science_1.webp";
+let imgs2="Images/insite_451_0117_1.webp";
+let num1=0;
+let num2=0;
+let num3=0;
 title.addEventListener("click",()=>{
-    content.classList.toggle("toggle");
-    let counter=0;
-    if(counter%2==0)
+     content.classList.toggle("toggle");
+    if(num1%2==0)
     {
-        image.src=img1;
+      image.src=imgs;  
     }
-    else{
+    else
+    {
         image.src=img0;
     }
+    num1++;
+    
 })
 title1.addEventListener("click",()=>{
     content1.classList.toggle("toggle");
+    if(num2%2==0)
+    {
+      image.src=imgs1;  
+    }
+    else
+    {
+        image.src=img0;
+    }
+    num2++;
 })
 title2.addEventListener("click",()=>{
     content2.classList.toggle("toggle");
+    if(num2%2==0)
+    {
+      image.src=imgs2;  
+    }
+    else
+    {
+        image.src=img0;
+    }
+    num2++;
 })
