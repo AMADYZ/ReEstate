e4170efb20d5d8fceed614a44909e6b377c80126
Mario Magdy <mariomagdyfa@gmail.com>
2023-03-12 19:45:24 +0200

    added

--- box.js
+++ box.js
@@ -2,12 +2,25 @@ const title=document.querySelector(".example.title");
 const content=document.querySelector(".example.content");
 const title1=document.querySelector(".example1.title1");
 const content1=document.querySelector(".example1.content1");
-const img1=document.querySelector(".inRsection6");
-
+const title2=document.querySelector(".example2.title2");
+const content2=document.querySelector(".example2.content2");
+let image=document.getElementById("img1");
+let img1="Images/DSC06832_3.webp";
+let img0="Images/accordion__1_.jpg";
 title.addEventListener("click",()=>{
     content.classList.toggle("toggle");
-    
+    let counter=0;
+    if(counter%2==0)
+    {
+        image.src=img1;
+    }
+    else{
+        image.src=img0;
+    }
 })
 title1.addEventListener("click",()=>{
     content1.classList.toggle("toggle");
+})
+title2.addEventListener("click",()=>{
+    content2.classList.toggle("toggle");
 })
