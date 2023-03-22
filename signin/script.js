027e5a1b63704d81badc31d5e74fab070348703d
Youssef Sherif <youssef.sherif03@gmail.com>
2023-03-23 00:34:30 +0200

    2 validations

--- signin/script.js
+++ signin/script.js
@@ -7,3 +7,31 @@ signupButton.addEventListener('click',()=>{
 signinButton.addEventListener('click',()=>{
     container.classList.remove("right1-pannel-active");
 })
+function validateUserName(field){
+    if(field=='')
+    {
+        document.getElementById('nameErr').innerHTML='You Must Enter a UserName.';
+        document.getElementById('er1').style.opacity='1';
+        return false;
+    }
+    else{
+    document.getElementById('nameErr').innerHTML='';
+    document.getElementById('er1').style.opacity='0';
+    return true;
+    }
+}
+function validateEmail(input) {
+
+    const validRegex = /^(.+)@(.+)$/;
+  
+    if (!validRegex.test(input) ) {
+        document.getElementById('emailErr').innerHTML='Check your email.';
+        document.getElementById('er2').style.opacity='1';
+        return false;
+    }
+    else{
+    document.getElementById('emailErr').innerHTML='';
+    document.getElementById('er2').style.opacity='0';
+    return true;
+    }
+}
