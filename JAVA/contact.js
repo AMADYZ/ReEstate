47be3882b3a7868e575a41fb9fccb5459b1b182d
Mario Magdy <mariomagdyfa@gmail.com>
2023-03-23 20:30:48 +0200

    added

--- JAVA/contact.js
+++ JAVA/contact.js
@@ -1,3 +1,10 @@
+let check=localStorage.getItem('check');
+localStorage.clear();
+if(check)
+{
+    document.getElementById("searchnav").style="display:inline";
+    document.getElementById("Signupnav").style="display:none";
+}
 let intro=document.querySelector('.intro');
 setTimeout(()=>
         {
