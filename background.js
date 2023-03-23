15c2ba86e935da5ca05f7d0a031d75b29a5342e2
Mario Magdy <mariomagdyfa@gmail.com>
2023-03-23 03:16:54 +0200

    addd

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
+let num;
 function changeBackground(){
-    if(window.scrollY>window.innerHeight*7.8){
+    
+    if(window.scrollY>window.innerHeight*num){
         document.body.classList.add('bg-color');
 
     }
@@ -9,4 +11,31 @@ function changeBackground(){
 
 }
 
+
 window.addEventListener('scroll',changeBackground);
+
+
+
+function myFunction(x) {
+    if (x.matches) {
+    num=2.6;
+    }
+    else{
+        num=7.8;
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+var x = window.matchMedia("(max-width: 350px)");
+myFunction(x);
+x.addListener(myFunction);
