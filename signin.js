4daf102f2bc49c3360c924f4e6721746e9c2c8a2
Mario Magdy <mariomagdyfa@gmail.com>
2023-03-23 14:04:58 +0200

    final signform

--- signin.js
+++ signin.js
@@ -1,7 +1,69 @@
+
+
+
+
+
+let ongo;
 function in_validate(form1){
-
-
-
-
-    
+    let fail1,fail2;
+     fail1=validateUserName1(form1.username_in.value);
+     fail2=validatepass1(form1.password_in.value);
+    if(fail1&&fail2)
+    {
+        return true;
+    }
+    else
+       return false; 
 }
+
+
+function validateUserName1(field){
+   
+    if(field=='')
+    {
+        document.getElementById('nameErr_in').innerHTML='Invalid UserName';
+        document.getElementById('er1_in').style.opacity='1';
+        return false;
+    }
+    else if(field!='user'&&field!='admin')
+    {
+        document.getElementById('nameErr_in').innerHTML='Invalid UserName';
+        document.getElementById('er1_in').style.opacity='1';
+        return false; 
+    }
+    else{
+    document.getElementById('nameErr_in').innerHTML='';
+    document.getElementById('er1_in').style.opacity='0';
+    ongo=field;
+    return true;
+    }
+}
+
+function validatepass1(field){
+   
+    if(field=='')
+    {
+        document.getElementById('passErr_in').innerHTML='Invalid Password';
+        document.getElementById('er2_in').style.opacity='1';
+        return false;
+    }
+    else if(ongo=='user'&&field!='123')
+    {
+        document.getElementById('passErr_in').innerHTML='Invalid Password';
+        document.getElementById('er2_in').style.opacity='1';
+        return false; 
+    }
+    else if(ongo=='admin'&&field!='1234')
+    {
+        document.getElementById('passErr_in').innerHTML='Invalid Password';
+        document.getElementById('er2_in').style.opacity='1';
+        return false; 
+    }
+    else{
+    document.getElementById('passErr_in').innerHTML='';
+    document.getElementById('er2_in').style.opacity='0';
+    return true;
+    }
+}
+
+
